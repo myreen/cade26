daa6e6dbc445b0604632f05eabc4e95df17eda6c
Magnus Myreen <magnus.myreen@cl.cam.ac.uk>
2017-02-16 21:16:53 +0100

    Add deadline ext and CASC, remove validate links

--- build.py
+++ build.py
@@ -28,35 +28,49 @@ CONTENT-HTML
 <div style="width: 100%; text-align:left; padding-left: 15px; padding-top: 10px; padding-bottom: 10px;">CADE-26 is grateful for sponsorship from:</div>
 <a href="https://www.microsoft.com/en-us/research/"><img src="msr.png" style="width: 200px; padding-left:30px;" alt="Microsoft Research"/></a>
 <a href="http://international.goteborg.se/"><img src="gbg.png" style="width: 200px; padding-left:30px;" alt="Gothenburg City"/></a>
-<a href="http://www.chalmers.se/en/areas-of-advance/ict/Pages/default.aspx"><img src="aoa.png" style="width: 280px; padding-left:30px;" alt="Information and Communication Technologies Chalmers Area of Advance"/></a><br><br>
-<a style="color:grey;" href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fwww.cse.chalmers.se%2F~myreen%2Fcade-26%2FFILENAME-DOT-HTML">validate</a>
+<a href="http://www.chalmers.se/en/areas-of-advance/ict/Pages/default.aspx"><img src="aoa.png" style="width: 280px; padding-left:30px;" alt="Information and Communication Technologies Chalmers Area of Advance"/></a><br>
 </footer>
 </body>
 </html>"""
 
+validate_link = """ - <a style="color:grey;" href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fwww.cse.chalmers.se%2F~myreen%2Fcade-26%2FFILENAME-DOT-HTML">FILENAME-DOT-HTML</a>"""
+
 # a list, where each item is: filename, title, is-item-on-main-nav
 
-pages = [("index","Home",True),
-         ("workshops","Workshops &amp; Tutorials",True),
-         ("org","Organization",True),
-         ("venue","Venue &amp; Hotels",True)]
+pages = [("index","Home",True,True),
+         ("workshops","Workshops &amp; Tutorials",True,True),
+         ("http://www.cs.miami.edu/~tptp/CASC/26/","CASC",True,False),
+         ("org","Organization",True,True),
+         ("venue","Venue &amp; Hotels",True,True)]
 
 # generate html
 
 def get_menu(filename):
   str = '<ul>\n'
-  for (filename,title,in_menu) in pages:
-      if in_menu:
-          str += '<li><a href="'+filename+'.html">'+title+'</a></li>\n'
+  for (filename,title,in_menu,is_local) in pages:
+    if in_menu:
+      if is_local:
+        str += '<li><a href="'+filename+'.html">'+title+'</a></li>\n'
+      else:
+        str += '<li><a href="'+filename+'" target="_blank">'+title+'</a></li>\n'
   str += '</ul>\n'
   return str
 
-for (filename,title,in_menu) in pages:
-  with open(filename+'.txt', 'r') as content_file:
-     content = content_file.read()
-  html = format.replace(title_tag,"CADE-26: " + title)
-  html = html.replace(menu_tag,get_menu(filename))
-  html = html.replace(content_tag,content)
-  html = html.replace(filename_tag,filename+'.html')
-  with open(filename+'.html', 'w') as html_file:
-     html_file.write(html)
+validate_html = ""
+
+for (filename,title,in_menu,is_local) in pages:
+  if (is_local):
+    with open(filename+'.txt', 'r') as content_file:
+      content = content_file.read()
+    html = format.replace(title_tag,"CADE-26: " + title)
+    html = html.replace(menu_tag,get_menu(filename))
+    html = html.replace(content_tag,content)
+    html = html.replace(filename_tag,filename+'.html')
+    with open(filename+'.html', 'w') as html_file:
+      html_file.write(html)
+    validate_html += validate_link.replace(filename_tag,filename+'.html') + '\n'
+
+validate_html = """<html><head><link rel="stylesheet" href="style2.css"></head><body><pre><b>Validate:</b>""" + '\n' + validate_html + "</pre></body></html>"
+
+with open('validate.html', 'w') as html_file:
+  html_file.write(validate_html)
